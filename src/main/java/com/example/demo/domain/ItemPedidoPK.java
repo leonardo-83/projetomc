65c9377c68b664fc7dfc4d2d60b0dfb2d353e30c
Leonardo Carpino <leonardocarpino@gmail.com>
2021-07-18 19:17:50 -0300

    Endpoit, pedido e {id} disponibilizados

--- src/main/java/com/example/demo/domain/ItemPedidoPK.java
+++ src/main/java/com/example/demo/domain/ItemPedidoPK.java
@@ -6,6 +6,9 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
 @Embeddable
 public class ItemPedidoPK implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +25,7 @@ public class ItemPedidoPK implements Serializable {
 	public Pedido getPedido() {
 		return pedido;
 	}
+	
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
