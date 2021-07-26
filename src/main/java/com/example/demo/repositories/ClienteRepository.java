31bb15bc9dbfd18c4e9e0e9c957f1c5759972de6
Leonardo Carpino <leonardocarpino@gmail.com>
2021-07-26 14:24:05 -0300

    Refresh token

--- src/main/java/com/example/demo/repositories/ClienteRepository.java
+++ src/main/java/com/example/demo/repositories/ClienteRepository.java
@@ -1,16 +1,16 @@
 package com.example.demo.repositories;
 
-import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
-	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	@Transactional(readOnly = true)
 	Cliente findByEmail(String email);
 	
 }
