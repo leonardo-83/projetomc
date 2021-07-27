965769c7f40d992aa28024639dbe3881cbe605bd
Leonardo Carpino <leonardocarpino@gmail.com>
2021-07-27 15:49:13 -0300

    Salvando arquivo no S3

--- src/main/java/com/example/demo/NovoprojetoApplication.java
+++ src/main/java/com/example/demo/NovoprojetoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.services.S3Service;
+
 @SpringBootApplication
 public class NovoprojetoApplication implements CommandLineRunner{
 
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NovoprojetoApplication.class, args);
@@ -16,6 +20,7 @@ public class NovoprojetoApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3Service.uploadFile("");
 			
 			 
 			 
